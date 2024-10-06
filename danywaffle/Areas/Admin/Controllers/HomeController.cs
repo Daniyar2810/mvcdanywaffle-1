6ae4b53c1ce72938f19f9da6746a32388bc89b65
Daniyar2810 <allabergenowd7@gmail.com>
2024-10-06 20:28:22 +0300

    Ana Sayfa

---
+++ danywaffle/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace danywaffle.Areas.Admin.Controllers;
+[Area("Admin")]
+public class HomeController : Controller
+{
+    public IActionResult AdminAnaSayfa()
+    {
+        return View();
+    }
+}
---
+++ danywaffle/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace danywaffle.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkımızda()
+        {
+            return View();
+        }
+        public ActionResult Iletişim()
+        {
+            return View();
+        }
+    }
+}
